8a2689740905bb626d979520763d3e2a9a5131c1
Brundha M <brundhamohan11@gmail.com>
2026-08-04 01:32:48 +0530

    Change database user and password in DBConnection
    Updated database credentials in DBConnection.java.

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/interview_tracker";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() {
         try {
